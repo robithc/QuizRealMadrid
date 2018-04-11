c04778720d25fcdf689c6b3ef028abdacc156385
robithc <robithc@gmail.com>
2018-04-11 16:10:37 +0300

    update

--- app/src/main/java/com/example/android/quizrealmadrid/MainActivity.java
+++ app/src/main/java/com/example/android/quizrealmadrid/MainActivity.java
@@ -1,97 +1,104 @@
 package com.example.android.quizrealmadrid;
 
-        import android.app.Activity;
-        import android.support.v7.app.AppCompatActivity;
-        import android.os.Bundle;
-        import android.view.View;
-        import android.widget.Button;
-        import android.widget.CheckBox;
-        import android.widget.EditText;
-        import android.widget.Toast;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.EditText;
+import android.widget.RadioButton;
+import android.widget.Toast;
 
-public class MainActivity extends Activity implements View.OnClickListener {
+public class MainActivity extends AppCompatActivity {
+
+    Button showScore;
     int endScore = 0;
-    Button buttonShowScore;
+    final View.OnClickListener showScoreButtonOnClickListener = new View.OnClickListener() {
+        public void onClick(final View v) {
+            checkAllAnswers();
+            Toast.makeText(MainActivity.this, "Your score is " + endScore  + "/4 correct answers",
+                    Toast.LENGTH_LONG).show();
+            resetScore();
+        }
+    };
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        buttonShowScore = (Button) findViewById(R.id.score_button);
-        buttonShowScore.setOnClickListener(MainActivity.this);
-
-
+        showScore = (Button) findViewById(R.id.score_button);
+        showScore.setOnClickListener(showScoreButtonOnClickListener);
     }
-
-    @Override
-    public void onClick(View v) {
-        //Your Logic
-        checkAllAnswers();
-    }
-
-
 
 
     //Check answer for question 1
 
     private void checkQuestionOne() {
-    EditText questionOneEditText = (EditText) findViewById(R.id.question_1_edit_text);
-    String questionOneAnswer = questionOneEditText.getText().toString();
-    String rightAnswer = "florentino perez";
-    if (questionOneAnswer.toLowerCase().equals(rightAnswer)){
-        endScore += 1;
+        EditText questionOneEditText = (EditText) findViewById(R.id.question_1_edit_text);
+        String questionOneAnswer = questionOneEditText.getText().toString();
+        String rightAnswer = "florentino perez";
+        if (questionOneAnswer.toLowerCase().equals(rightAnswer)) {
+            endScore += 1;
         }
+    }
 
-    }
-    public void onClickChechbox(View v) {
-        //Your Logic
-        checkAllAnswers();
-    }
+
     //Check answer for question 2
 
-    public void checkQuestionTwo( View v) {
+    private void checkQuestionTwo() {
+
         CheckBox checkBoxQuestionTwoAnswerOne = (CheckBox) findViewById(R.id.question_2_answer_1);
         CheckBox checkBoxQuestionTwoAnswerTwo = (CheckBox) findViewById(R.id.question_2_answer_2);
         CheckBox checkBoxQuestionTwoAnswerThree = (CheckBox) findViewById(R.id.question_2_answer_3);
         CheckBox checkBoxQuestionTwoAnswerFour = (CheckBox) findViewById(R.id.question_2_answer_4);
         CheckBox checkBoxQuestionTwoAnswerFive = (CheckBox) findViewById(R.id.question_2_answer_5);
+
         Boolean answerOneChecked = checkBoxQuestionTwoAnswerOne.isChecked();
         Boolean answerTwoChecked = checkBoxQuestionTwoAnswerTwo.isChecked();
         Boolean answerThreeChecked = checkBoxQuestionTwoAnswerThree.isChecked();
         Boolean answerFourChecked = checkBoxQuestionTwoAnswerFour.isChecked();
         Boolean answerFiveChecked = checkBoxQuestionTwoAnswerFive.isChecked();
-        String endScoreText = "Your score is " + endScore;
-        Toast.makeText(this,endScoreText, Toast.LENGTH_LONG).show();
 
-        if (answerTwoChecked & answerFourChecked){
-            endScore =+1;
+        if (answerTwoChecked && answerFourChecked && !answerOneChecked && !answerThreeChecked && !answerFiveChecked) {
+            endScore += 1;
+
         }
-
-
-
-    }
-
-
-    //Check answer for question 2
-
-    private void checkQuestionThree() {
-
-
     }
 
     //Check answer for question 3
 
-    private void checkQuestionFour() {
+    private void checkQuestionThree() {
 
-
+        RadioButton questionThreeAnswerThree = (RadioButton) findViewById(R.id.question_3_answer_3);
+        boolean isQuestionThreeAnswerThree = questionThreeAnswerThree.isChecked();
+        if (isQuestionThreeAnswerThree) {
+            endScore += 1;
+        }
     }
-    private void checkAllAnswers(){
+    //Check answer for question 4
+
+    private void checkQuestionFour() {
+        RadioButton questionFourAnswerTwo = (RadioButton) findViewById(R.id.question_4_answer_2);
+        boolean isQuestionFourAnswerTwo = questionFourAnswerTwo.isChecked();
+        if (isQuestionFourAnswerTwo) {
+            endScore += 1;
+        }
+    }
+    //Check all answers
+
+    private void checkAllAnswers() {
         checkQuestionOne();
-        //checkQuestionTwo(View viev);
+        checkQuestionTwo();
         checkQuestionThree();
         checkQuestionFour();
-        String endScoreText = "Your score is " + endScore;
-        Toast.makeText(this,endScoreText, Toast.LENGTH_LONG).show();
+    }
+
+    //Reset score
+
+    private void resetScore() {
+        endScore = 0;
     }
 
 }
